c0f90370103e10a7c9dbd528239adc484c2df23c
Chainyinghao <dyinghao@outlook.com>
2018-09-12 10:59:26 +0800

    push script name 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# date:2018/9/12
+# comment:有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if i!=j and j!=k and i!=k :
+                print(i,j,k)
+
