0b52781e4eb34e5f83101d4c012cced27faf5b03
chain <41147624+Chainyinghao@users.noreply.github.com>
2018-09-17 21:35:19 +0800

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# date:2018/9/16
+# date:2018/9/17
 # comment:求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个数字。例如2+22+222+2222+22222(此时共有5个数相加)，几个数相加由键盘控制。
 
-from functools import reduce       #听说Mr Guido大大不喜欢reduce这个累积函数？？？不能像2.x版本直接使用了，emmm
+from functools import reduce       #听说Mr Guido大大不喜欢reduce这个累积函数？？？所以删了它的全局命名空间不能像2.x版本直接使用了，emmm
 def cir_Num():
     temp = 0
     Sn = []
@@ -16,7 +16,7 @@ def cir_Num():
     for i in range(n-1):
         print("{} + ".format(Sn[i]), end=" ")
     print(Sn[-1])
-    Sn = reduce(lambda x, y: x + y, Sn)  # 感觉这里用lamba很骚，参考别人的
+    Sn = reduce(lambda x, y: x + y, Sn)  # 感觉这里用lamba函数是很骚的操作，参考别人的
     print("    = %d" % Sn)
 
 cir_Num()
