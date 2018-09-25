d4c42c094a04b3a3db93b70358e476d4467613c8
chain <41147624+Chainyinghao@users.noreply.github.com>
2018-09-25 21:07:04 +0800

    Update 19.py

--- 19.py
+++ 19.py
@@ -7,14 +7,16 @@ from urllib import request
 
 def fetch_data(url):
     with request.urlopen(url) as f:
-        return json.loads(f.read())
+        for i, j in f.getheaders():
+            print("%s:%s" %(i, j))
+        return json.loads(f.read().decode("utf-8"))
 
 URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
 data = fetch_data(URL)
+print("\n获取数据为：")
 print(data)
 assert data['query']['results']['channel']['location']['city'] == 'Beijing'
 print('ok')
-
 
 
 
