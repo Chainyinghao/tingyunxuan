44b0dd1c2f8475d4e5cca1a3216787d8a24d75e7
Chainyinghao <dyinghao@outlook.com>
2018-09-28 16:35:53 +0800

    20.py

---
+++ 20.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# date:2018/9/25
+# comment:递归二分法查找
+
+#模块bisect提供标准的二分法查找，只是想写写
+
+def search(sequance, number, lower = 0, upper = None):
+    if upper is None:
+        upper = len(sequance) - 1
+    if lower == upper:
+        assert number == sequance[upper]
+        return upper
+    else:
+        middle = (lower + upper) // 2
+        if number > sequance[middle]:
+            return search(sequance, number, middle+1, upper)
+        else:
+            return search(sequance, number, lower, middle)
+
+x = input("输入任意一组整数(空格隔开)：")
+seq = x.split(" ")   #这里的话，也就是说在py里面，一个变量可以保存多个值，《python基础教程》赋值那章有
+seq = [int(seq[i])for i in range(len(seq))]         #将列表中每个字符值转化为整形
+seq.sort()
+print(seq)
+y = eval(input("输入查找字符："))
+print("该字符位于：%s" % (search(seq, y)+1))
+
+
+
+
+
+
+
+
